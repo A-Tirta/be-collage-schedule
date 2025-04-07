32f8b488a95b233f94a282de186de0850c1d6e83
A-Tirta <tirtananda68@gmail.com>
2025-04-07 10:48:24 +0700

    main add middleware check jwt

---
+++ src/middleware/index.ts
@@ -0,0 +1,11 @@
+import * as dotenv from "dotenv";
+import { expressjwt } from "express-jwt";
+
+dotenv.config();
+
+export const JWTCheck = expressjwt({
+  secret: process.env.TOKEN as string,
+  algorithms: ["HS256"],
+}).unless({
+  path: ["/user/login", "/status", "/user/register"],
+});
