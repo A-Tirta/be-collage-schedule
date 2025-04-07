4ca01c74c5dfc2cc79cc1b841d72265edffbdddc
A-Tirta <tirtananda68@gmail.com>
2025-04-07 10:48:54 +0700

    main add routes

---
+++ src/routes/index.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+
+const routes = Router();
+
+/* Example */
+// import status from '../controller/status';
+// routes.use('/status', status);
+
+export default routes;
