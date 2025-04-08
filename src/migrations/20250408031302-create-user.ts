6772bcaf4426240c89bd9e20f7c00e3b0b901c8d
A-Tirta <tirtananda68@gmail.com>
2025-04-08 13:57:55 +0700

    main add migrations user

---
+++ src/migrations/20250408031302-create-user.ts
@@ -0,0 +1,34 @@
+import { QueryInterface, DataTypes } from "sequelize";
+
+export default {
+  up: async (queryInterface: QueryInterface): Promise<void> => {
+    await queryInterface.createTable("users", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      name: {
+        type: DataTypes.STRING,
+      },
+      email: {
+        type: DataTypes.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+      },
+    });
+  },
+
+  down: async (queryInterface: QueryInterface): Promise<void> => {
+    await queryInterface.dropTable("Users");
+  },
+};
